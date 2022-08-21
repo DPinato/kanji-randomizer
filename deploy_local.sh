4d7c4105a255a47e09963da5606e9686a86aec85
dpinato <dvdpnt@hotmail.it>
2022-08-21 20:13:28 +0100

    Added creating temp cache folder

--- deploy_local.sh
+++ deploy_local.sh
@@ -4,7 +4,7 @@
 go mod tidy
 go build -o kanji-randomizer kanji-randomizer.go
 
-# create the /usr/local/bin/twitchjoiner directory and move executable there
+# create the /usr/local/bin/kanji-randomizer directory and move executable there
 TJ_DIR="/usr/local/bin/kanji-randomizer"
 if [ ! -d $TJ_DIR ]; then
   sudo mkdir -p $TJ_DIR;
@@ -13,3 +13,10 @@ fi
 sudo chown pi $TJ_DIR
 cp -t $TJ_DIR/ kanji-randomizer
 rm kanji-randomizer
+
+# create directory where cached kanji lists will be stored
+KANJI_LIST_DIR="/tmp/kanji-randomizer"
+if [ ! -d $KANJI_LIST_DIR ]; then
+  sudo mkdir -p $KANJI_LIST_DIR;
+fi
+sudo chown pi $KANJI_LIST_DIR
